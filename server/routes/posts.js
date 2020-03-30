458df3b14aafc5154ff84c8268fb5c47ecb59f4f
qqww08 <qqwwee08@naver.com>
2020-03-31 02:13:04 +0900

    test

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -4,8 +4,6 @@ const { checkLoggedIn } = require("../middleware/checkLoggedIn");
 const { Post } = require("../models/Post");
 
 router.post("/wr", checkLoggedIn, (req, res) => {
-  //회원 가입 할떄 필요한 정보들을  client에서 가져오면
-  //그것들을  데이터 베이스에 넣어준다.
   const post = new Post(req.body);
   post.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
