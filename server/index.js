6ff9902f4c5c7ae3db9298a34a96a9e0a4d69c82
qqww08 <qqwwee08@naver.com>
2020-04-06 00:05:23 +0900

    test

--- server/index.js
+++ server/index.js
@@ -27,11 +27,15 @@ app.use("/api/posts", require("./routes/posts"));
 app.use("/api/users", require("./routes/users"));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
+app.get("/api/greeting", (req, res) => {
+  res.send("Hello World!");
+});
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
 const port = process.env.PORT || 5000;
