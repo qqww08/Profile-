1b00ff5f8ca5e762ce3547add8856c61da862348
qqww08 <qqwwee08@naver.com>
2020-04-06 01:54:21 +0900

    test

--- server/index.js
+++ server/index.js
@@ -2,21 +2,18 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-// const config = require("./config/key");
+const config = require("./config/key");
 
 //application/x-www-form-urlencoded
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://SDH:tlseog08!@cluster0-jxe5q.mongodb.net/test?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
