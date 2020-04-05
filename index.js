bd136ec039f4106e26a2edfe39ae71a57d2dc377
qqww08 <qqwwee08@naver.com>
2020-04-06 03:11:13 +0900

    test

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
 
-//application/x-www-form-urlencoded
-
 const mongoose = require("mongoose");
 mongoose
   .connect(config.mongoURI, {
@@ -22,18 +20,6 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use("/api/posts", require("./routes/posts"));
 app.use("/api/users", require("./routes/users"));
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
-
-app.get("/api/greeting", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
