bf060ee6cf3d4766865ff821eb5722850d89943f
qqww08 <qqwwee08@naver.com>
2020-04-05 18:00:39 +0900

    1

--- client/src/pages/Main/Profile.js
+++ client/src/pages/Main/Profile.js
@@ -13,12 +13,7 @@ function Profile() {
 
       <div className="profile_flex">
         <img src={images1} alt="pro" className="profile_img" />
-        <ul>
-          <li>Profile</li>
-          <li>신대현 DaeHyun Shin</li>
-          <li>1994.05.10</li>
-          <li>울산과학대학 컴퓨터정보학과 졸업</li>
-        </ul>
+        <ul></ul>
       </div>
     </>
   );
