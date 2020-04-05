d9de88e9a752abc76120c563c283f7575ea0c5db
qqww08 <qqwwee08@naver.com>
2020-04-05 23:10:29 +0900

    test

--- index.js
+++ index.js
@@ -28,4 +28,6 @@ app.use("/api/users", require("./routes/users"));
 
 const port = process.env.PORT || 5000;
 console.log("현재 포트" + process.env.PORT);
-app.listen(5000, () => console.log(`Example app listening on port ${port}!`));
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
