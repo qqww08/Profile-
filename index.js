524884dc1fe10dd68d222ffc44addcbd789cfc04
qqww08 <qqwwee08@naver.com>
2020-04-08 19:28:48 +0900

    heroku

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
+const path = require("path");
 
 const mongoose = require("mongoose");
 mongoose
@@ -20,6 +21,15 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use("/api/posts", require("./routes/posts"));
 app.use("/api/users", require("./routes/users"));
+app.use("/", routes);
+
+if (process.env.NODE_EUV === "production") {
+  app.use(express.static("client/bulid"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
