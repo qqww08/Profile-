8bc24ebea4ae51e3006d64db4a8bf2cee4b5a88e
qqww08 <qqwwee08@naver.com>
2020-04-08 21:43:00 +0900

    test2

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ app.use("/api/users", require("./routes/users"));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/bulid"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  // });
 }
 
 const port = process.env.PORT || 5000;
