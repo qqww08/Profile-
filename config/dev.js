79b034e08cd10839c22b3df16240167be0a7e840
qqww08 <qqwwee08@naver.com>
2020-04-08 21:58:33 +0900

    test2

--- config/dev.js
+++ config/dev.js
@@ -1,3 +1,4 @@
-module.exports ={
-    mongoURI:'mongodb+srv://SDH:tlseog08!@cluster0-jxe5q.mongodb.net/test?retryWrites=true&w=majority'
-}
+module.exports = {
+  mongoURI:
+    "mongodb+srv://SDH:tlseog08!@cluster0-jxe5q.mongodb.net/test?retryWrites=true&w=majority",
+};
