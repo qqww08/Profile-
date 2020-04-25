05a01bc12cb985825525983d863d0e4411f5220b
qqww08 <qqwwee08@naver.com>
2020-04-25 22:03:35 +0900

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const config = require("./config/key");
+// const config = require("./config/key");
 const path = require("path");
 
 const mongoose = require("mongoose");
