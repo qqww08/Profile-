ffb9cb6eacf6accaa83449879790150db1468fc0
qqww08 <qqwwee08@naver.com>
2020-05-13 03:19:04 +0900

    ec2 test

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,8 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target:
-        "http://ec2-13-125-131-15.ap-northeast-2.compute.amazonaws.com:5000",
+      target: "http://localhost:5000",
       changeOrigin: true,
     })
   );
