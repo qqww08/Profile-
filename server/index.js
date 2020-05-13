347a81079f6dfa2901c4cbc6e0f5c9871605c113
qqww08 <qqwwee08@naver.com>
2020-05-13 14:37:10 +0900

    디렉토리 정리

---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+// const config = require("./config/key");
+const path = require("path");
+// const cors = require("cors");
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://SDH:tlseog08!@cluster0-jxe5q.mongodb.net/test?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+// app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use("/api/posts", require("./routes/posts"));
+app.use("/api/users", require("./routes/users"));
+app.use("/uploads", express.static("uploads"));
+app.use("/api/comment", require("./routes/comment"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
